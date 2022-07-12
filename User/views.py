3cae9a050df48a283a327578b68de4236e0b9103
Rabby-Shanto <rabby1112@student.nstu.edu.bd>
2022-07-13 01:05:15 +0600

    Adding Account registration with email verification[Day-1]

--- User/views.py
+++ User/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 from User.models import UserAcc
 
 from .forms import RegistrationForm
+from django.contrib import messages,auth
+from django.contrib.auth.decorators import login_required
 
+#verification mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 # Create your views here.
 
 
@@ -23,6 +32,20 @@ def register(request):
             user = UserAcc.objects.create_user(first_name=first_name,last_name=last_name,email=email,username=username,password=password)
             user.phone_number= phone_number
             user.save()
+            # User Activation
+            current_site = get_current_site(request)
+            mail_subject = "Please activate your account"
+            message = render_to_string('accounts/verification_mail.html',{
+                'user' : user,
+                'domain' : current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user)
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
+            messages.success(request,"Registration Successful")
+            return redirect('register')
 
 
     else:
@@ -39,6 +62,30 @@ def register(request):
 
 
 def login(request):
+
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email,password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            messages.success(request,'You are now logged in')
+            return redirect('home')
+
+        else:
+            messages.error(request,'Invalid email or password!')
+            return redirect('login')
+
     return render(request,'accounts/login.html')
+
+
+
+@login_required(login_url='login')
+
 def logout(request):
-    return HttpResponse("Hello User")
+    auth.logout(request)
+    messages.success(request,"You are logged out")
+
+    return redirect('login')
