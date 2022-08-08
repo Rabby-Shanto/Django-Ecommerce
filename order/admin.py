16bb385cac65f725f9d2e93b116421c7d075bf1b
Rabby_Shanto <rabby1112@student.nstu.edu.bd>
2022-08-08 23:45:56 +0600

    created order models

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Order,OrderedProduct,Payment
 
 # Register your models here.
+
+
+admin.site.register(Order)
+admin.site.register(OrderedProduct)
+admin.site.register(Payment)
+
