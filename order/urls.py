10fac33869ba2f0f62a3236d72ca464131fb763a
Rabby-Shanto <rabby1112@student.nstu.edu.bd>
2022-09-27 01:42:00 +0600

    Added paypal payment gateway

--- order/urls.py
+++ order/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('place_order/',views.place_order,name="place_order"),
-    path('payments/',views.payment,name='payment'),
+    path('payments/',views.payment,name='payments'),
 
 ]
--- order/views.py
+++ order/views.py
@@ -1,8 +1,31 @@
+import json
 from django.shortcuts import render,redirect
 from .models import Order,OrderedProduct,Payment
 from cart.models import Cart_item
 from .forms import OrderForm
 import datetime
+
+
+def payment(request):
+    body =  json.loads(request.body)
+    print(body)
+    order = Order.objects.get(user=request.user,is_ordered=False,order_number=body['orderID'])
+    # storing transaction details
+    payment = Payment(
+        user = request.user,
+        payment_id = body['transactionID'],
+        payment_method = body['payment_method'],
+        amount_paid = order.order_total,
+        status = body['status'],
+
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True #After payment successful,it should be true
+    order.save()
+
+    return render(request,'orders/payments.html')
 
 def place_order(request,total=0,quantity=0):
     current_user = request.user
@@ -77,7 +100,6 @@ def place_order(request,total=0,quantity=0):
 
 
 
-def payment(request):
-    return render(request,'orders/payments.html')
+
 
 
